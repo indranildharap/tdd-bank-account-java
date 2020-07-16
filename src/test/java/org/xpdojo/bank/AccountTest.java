2ad8331990d4be6a33d6370d56a67dbb2c839e1c
Indranil Dharap <indranildharap@gmail.com>
2020-07-16 19:38:05 +0530

    Added test case to test checkBalance

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -2,14 +2,15 @@ package org.xpdojo.bank;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
+import org.xpdojo.bank.Account;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
 
     @Test
-	@Disabled
+
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+        Account account = new Account();
+        assertThat(account.checkBalance(10)).isEqualTo(0);
     }
 }
